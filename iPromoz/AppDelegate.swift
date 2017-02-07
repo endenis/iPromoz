994b600cb094e5a02a8097befa3e69fec6b5c865
endenis <endenis@gmail.com>
2017-02-07 23:44:16 +0100

    Initial Commit

---
+++ iPromoz/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  iPromoz
+//
+//  Created by Denis Engels on 07/02/2017.
+//  Copyright © 2017 Denis Engels. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ iPromoz/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  iPromoz
+//
+//  Created by Denis Engels on 07/02/2017.
+//  Copyright © 2017 Denis Engels. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
