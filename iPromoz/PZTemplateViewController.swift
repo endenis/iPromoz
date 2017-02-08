307862e73bd98420c849c3ed86618b240f8db290
Denis <endenis@gmail.com>
2017-02-08 14:14:00 +0100

    Properly update NSImageView

--- iPromoz/PZTemplateViewController.swift
+++ iPromoz/PZTemplateViewController.swift
@@ -8,7 +8,6 @@ import Cocoa
 class PZTemplateViewController: NSViewController {
 
     @IBOutlet var instructionLabel: NSTextField?
-    @IBOutlet var templateImageView: NSImageView?
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -24,14 +23,17 @@ extension PZTemplateViewController: PZTemplateViewDelegate {
 
     func nothingToDoState() {
         instructionLabel?.isHidden = false
-        templateImageView?.image = nil
-        Swift.print("nothing")
+        if let templateView = self.view as? PZTemplateView {
+            templateView.image = nil
+        }
     }
     
     func workingWithATemplateState(_ templateUrl: URL) {
         instructionLabel?.isHidden = true
         Swift.print(templateUrl)
-        templateImageView?.image = NSImage.init(contentsOf: templateUrl)
+        if let templateView = self.view as? PZTemplateView {
+            templateView.image = NSImage.init(contentsOf: templateUrl)
+        }
     }
     
 }
