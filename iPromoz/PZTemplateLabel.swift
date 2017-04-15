8b26a4ffac19a09048b08a217a6d2b3ba5298974
endenis <endenis@gmail.com>
2017-04-15 20:43:44 +0200

    Laggy dragging of text label

--- iPromoz/PZTemplateLabel.swift
+++ iPromoz/PZTemplateLabel.swift
@@ -10,6 +10,9 @@ import Cocoa
 
 class PZTemplateLabel: NSTextField {
 
+    var clickedDownX: CGFloat = 0
+    var clickedDownY: CGFloat = 0
+
     override func draw(_ dirtyRect: NSRect) {
         super.draw(dirtyRect)
 
@@ -20,12 +23,32 @@ class PZTemplateLabel: NSTextField {
         Swift.print("mouse entered")
         Swift.print(self.frame)
         self.drawsBackground = true
-        self.backgroundColor = NSColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 0.9)
+        self.backgroundColor = NSColor(red: 0.5, green: 0.0, blue: 0.5, alpha: 0.2)
     }
 
     override func mouseExited(with event: NSEvent) {
         Swift.print("mouse exited")
         self.drawsBackground = false
+    }
+
+    override func mouseDown(with event: NSEvent) {
+        Swift.print("mouse down")
+        let position = self.convert(event.locationInWindow, from: nil)
+        self.clickedDownX = position.x
+        self.clickedDownY = position.y
+    }
+    
+    override func mouseDragged(with event: NSEvent) {
+        let position = self.convert(event.locationInWindow, from: nil)
+        Swift.print("mouse dragged \(position) while the frame is \(self.frame)")
+        self.frame.origin.x += position.x - self.clickedDownX
+        self.frame.origin.y -= position.y - self.clickedDownY
+        self.clickedDownX = position.x
+        self.clickedDownY = position.y
+    }
+    
+    override func mouseUp(with event: NSEvent) {
+        Swift.print("mouse up")
     }
 
     func removeTrackingAreas() {
