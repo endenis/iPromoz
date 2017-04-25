a1800ca134871e6e1f8aa6e5fb6b93955701ee5d
endenis <endenis@gmail.com>
2017-04-25 23:23:19 +0200

    Changing label colour + clean up

--- iPromoz/PZTemplateViewController.swift
+++ iPromoz/PZTemplateViewController.swift
@@ -10,7 +10,6 @@ class PZTemplateViewController: NSViewController {
     @IBOutlet var instructionLabel: NSTextField?
     @IBOutlet var exampleLabel    : PZTemplateLabel?
 
-    var overlayView: NSView?
     var fontSize: CGFloat = 20.0
     var ratioX: CGFloat = 0.5
     var ratioY: CGFloat = 0.5
@@ -19,47 +18,28 @@ class PZTemplateViewController: NSViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
+        let colorPanel = NSColorPanel.shared()
+        colorPanel.setTarget(self)
+        colorPanel.setAction(#selector(colorDidChange(sender:)))
+        colorPanel.isContinuous = true
+
         if let templateView = self.view as? PZTemplateView {
             templateView.delegate = self
             templateView.postsFrameChangedNotifications = true
-            //overlayView = NSView.init()
-            //overlayView!.isHidden = true
-            //overlayView!.wantsLayer = true
-            //overlayView!.layer?.backgroundColor = CGColor.init(red: 0, green: 100, blue: 0, alpha: 0.42)
-            //templateView.addSubview(overlayView!)
             let nc = NotificationCenter.default
             nc.addObserver(forName:NSNotification.Name.NSViewFrameDidChange, object: templateView, queue: nil, using: catchNotification)
         }
         let fontManager = NSFontManager.shared()
         fontManager.target = self
     }
-    
-    override func changeFont(_ sender: Any?) {
-        if let theLabel = exampleLabel {
-            let oldFont = theLabel.font!
-            let fontManager = sender as! NSFontManager
-            let newFont = fontManager.convert(oldFont)
-            self.fontSize = newFont.pointSize
-            theLabel.font = newFont
-            theLabel.sizeToFit()
-            updateSizes()
-        }
-    }
 
     func catchNotification(_: Notification) -> Void {
         updateSizes()
     }
 
-    func changeAttributes(_ sender: Any?) {
-        if let _ = exampleLabel {
-            Swift.print("changeAttributes from \(sender)")
-        }
-    }
-
     func updateSizes() -> Void {
         if let templateView = self.view as? PZTemplateView, let templateRectangle: NSRect = templateView.imageRectangle(), let templateRatio: CGFloat = templateView.imageResizeRatio() {
             updateExampleLabel(templateRectangle: templateRectangle, templateRatio: templateRatio)
-            overlayView?.frame = templateRectangle
         }
     }
 
@@ -79,6 +59,14 @@ class PZTemplateViewController: NSViewController {
         self.alignmentCoefficient = CGFloat(sender.selectedSegment)
         updateSizes()
     }
+
+    func colorDidChange(sender: Any?) {
+        if let colorPanel = sender as? NSColorPanel, let theLabel = exampleLabel {
+            print("Selected colour is: \(colorPanel.color)")
+            theLabel.textColor = colorPanel.color
+        }
+    }
+
 }
 
 extension PZTemplateViewController: PZTemplateViewDelegate {
@@ -86,7 +74,6 @@ extension PZTemplateViewController: PZTemplateViewDelegate {
     func nothingToDoState() {
         instructionLabel?.isHidden = false
         exampleLabel?.isHidden = true
-        overlayView?.isHidden = true
         if let templateView = self.view as? PZTemplateView {
             templateView.image = nil
         }
@@ -96,13 +83,11 @@ extension PZTemplateViewController: PZTemplateViewDelegate {
     func workingWithATemplateState(_ templateUrl: URL) {
         instructionLabel?.isHidden = true
         exampleLabel?.isHidden = false
-        overlayView?.isHidden = false
         Swift.print(templateUrl)
         if let templateView = self.view as? PZTemplateView {
             templateView.image = NSImage.init(contentsOf: templateUrl)
             if let templateRectangle: NSRect = templateView.imageRectangle(), let templateRatio: CGFloat = templateView.imageResizeRatio() {
                 updateExampleLabel(templateRectangle: templateRectangle, templateRatio: templateRatio)
-                overlayView?.frame = templateRectangle
             }
         }
         if let theLabel = exampleLabel {
